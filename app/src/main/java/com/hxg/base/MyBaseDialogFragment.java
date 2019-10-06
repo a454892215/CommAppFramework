2d9b6d9752f602634547fb8190567010b1a689f6
LiuPan <1573802540@qq.com>
2019-10-06 19:38:45 +0800

    增加base类

---
+++ app/src/main/java/com/hxg/base/MyBaseDialogFragment.java
@@ -0,0 +1,16 @@
+package com.hxg.base;
+
+import com.common.base.BaseDialogFragment;
+
+/**
+ * Author: Pan
+ * 2019/10/6
+ * Description:
+ */
+public abstract class MyBaseDialogFragment extends BaseDialogFragment {
+
+    @Override
+    protected void initView() {
+
+    }
+}
